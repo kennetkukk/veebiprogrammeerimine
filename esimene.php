1f21d391d12ebce1e08e8bc4bb3fb4b98caeca50
Kennet Kukk <kennetkukk@users.noreply.github.com>
2017-09-11 10:37:02 +0300

    HTML koodile lisatud esimene php osa

---
+++ esimene.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Kennet Kukk veebriprogremise asjad</title>
+</head>
+<body>
+    <h1>Kennet Kukk</h1>
+    <p>See veebileht on loodud veebiprogrammeerimise kursusel ning ei sisalda mingisugust tõsiseltvõetavalt sisu.</p>
+    <p>TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT.</p>
+	
+	<?php
+	echo "Tana on vastik ilm!";
+	?>
+    
+</body>
+</html>
